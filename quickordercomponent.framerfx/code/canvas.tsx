f9e0f7e59da754e537ff1ab3968b47ada0ec5c38
Elias Julian <eliasneverlost@gmail.com>
2019-04-20 15:03:00 -0500

    Add .framerfx to repo

---
+++ quickordercomponent.framerfx/code/canvas.tsx
@@ -0,0 +1,5 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Button = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_XLKBgddHe", {}, 250,50);
+export const Card = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_Pamq8RY9I", {}, 400,150);
